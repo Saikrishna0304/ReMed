0c564d585619b7f9a5cee0280bc9bea4474cff33
Sai Krishna <saikrishna.pentina@gmail.com>
2026-09-01 02:08:30 +0530

    login and step counter added

---
+++ app/build/generated/ksp/debug/java/com/example/remed/data/StepDao_Impl.java
@@ -0,0 +1,268 @@
+package com.example.remed.data;
+
+import android.database.Cursor;
+import android.os.CancellationSignal;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.room.CoroutinesRoom;
+import androidx.room.EntityDeletionOrUpdateAdapter;
+import androidx.room.EntityInsertionAdapter;
+import androidx.room.RoomDatabase;
+import androidx.room.RoomSQLiteQuery;
+import androidx.room.util.CursorUtil;
+import androidx.room.util.DBUtil;
+import androidx.sqlite.db.SupportSQLiteStatement;
+import java.lang.Class;
+import java.lang.Exception;
+import java.lang.Object;
+import java.lang.Override;
+import java.lang.String;
+import java.lang.SuppressWarnings;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.Callable;
+import javax.annotation.processing.Generated;
+import kotlin.Unit;
+import kotlin.coroutines.Continuation;
+import kotlinx.coroutines.flow.Flow;
+
+@Generated("androidx.room.RoomProcessor")
+@SuppressWarnings({"unchecked", "deprecation"})
+public final class StepDao_Impl implements StepDao {
+  private final RoomDatabase __db;
+
+  private final EntityInsertionAdapter<StepLog> __insertionAdapterOfStepLog;
+
+  private final EntityInsertionAdapter<StepSettings> __insertionAdapterOfStepSettings;
+
+  private final EntityDeletionOrUpdateAdapter<StepLog> __updateAdapterOfStepLog;
+
+  public StepDao_Impl(@NonNull final RoomDatabase __db) {
+    this.__db = __db;
+    this.__insertionAdapterOfStepLog = new EntityInsertionAdapter<StepLog>(__db) {
+      @Override
+      @NonNull
+      protected String createQuery() {
+        return "INSERT OR REPLACE INTO `step_logs` (`userId`,`date`,`count`) VALUES (?,?,?)";
+      }
+
+      @Override
+      protected void bind(@NonNull final SupportSQLiteStatement statement,
+          @NonNull final StepLog entity) {
+        statement.bindString(1, entity.getUserId());
+        statement.bindString(2, entity.getDate());
+        statement.bindLong(3, entity.getCount());
+      }
+    };
+    this.__insertionAdapterOfStepSettings = new EntityInsertionAdapter<StepSettings>(__db) {
+      @Override
+      @NonNull
+      protected String createQuery() {
+        return "INSERT OR REPLACE INTO `step_settings` (`userId`,`dailyGoal`) VALUES (?,?)";
+      }
+
+      @Override
+      protected void bind(@NonNull final SupportSQLiteStatement statement,
+          @NonNull final StepSettings entity) {
+        statement.bindString(1, entity.getUserId());
+        statement.bindLong(2, entity.getDailyGoal());
+      }
+    };
+    this.__updateAdapterOfStepLog = new EntityDeletionOrUpdateAdapter<StepLog>(__db) {
+      @Override
+      @NonNull
+      protected String createQuery() {
+        return "UPDATE OR ABORT `step_logs` SET `userId` = ?,`date` = ?,`count` = ? WHERE `userId` = ? AND `date` = ?";
+      }
+
+      @Override
+      protected void bind(@NonNull final SupportSQLiteStatement statement,
+          @NonNull final StepLog entity) {
+        statement.bindString(1, entity.getUserId());
+        statement.bindString(2, entity.getDate());
+        statement.bindLong(3, entity.getCount());
+        statement.bindString(4, entity.getUserId());
+        statement.bindString(5, entity.getDate());
+      }
+    };
+  }
+
+  @Override
+  public Object insertLog(final StepLog log, final Continuation<? super Unit> $completion) {
+    return CoroutinesRoom.execute(__db, true, new Callable<Unit>() {
+      @Override
+      @NonNull
+      public Unit call() throws Exception {
+        __db.beginTransaction();
+        try {
+          __insertionAdapterOfStepLog.insert(log);
+          __db.setTransactionSuccessful();
+          return Unit.INSTANCE;
+        } finally {
+          __db.endTransaction();
+        }
+      }
+    }, $completion);
+  }
+
+  @Override
+  public Object insertSettings(final StepSettings settings,
+      final Continuation<? super Unit> $completion) {
+    return CoroutinesRoom.execute(__db, true, new Callable<Unit>() {
+      @Override
+      @NonNull
+      public Unit call() throws Exception {
+        __db.beginTransaction();
+        try {
+          __insertionAdapterOfStepSettings.insert(settings);
+          __db.setTransactionSuccessful();
+          return Unit.INSTANCE;
+        } finally {
+          __db.endTransaction();
+        }
+      }
+    }, $completion);
+  }
+
+  @Override
+  public Object updateLog(final StepLog log, final Continuation<? super Unit> $completion) {
+    return CoroutinesRoom.execute(__db, true, new Callable<Unit>() {
+      @Override
+      @NonNull
+      public Unit call() throws Exception {
+        __db.beginTransaction();
+        try {
+          __updateAdapterOfStepLog.handle(log);
+          __db.setTransactionSuccessful();
+          return Unit.INSTANCE;
+        } finally {
+          __db.endTransaction();
+        }
+      }
+    }, $completion);
+  }
+
+  @Override
+  public Flow<StepLog> getLogForDate(final String userId, final String date) {
+    final String _sql = "SELECT * FROM step_logs WHERE userId = ? AND date = ?";
+    final RoomSQLiteQuery _statement = RoomSQLiteQuery.acquire(_sql, 2);
+    int _argIndex = 1;
+    _statement.bindString(_argIndex, userId);
+    _argIndex = 2;
+    _statement.bindString(_argIndex, date);
+    return CoroutinesRoom.createFlow(__db, false, new String[] {"step_logs"}, new Callable<StepLog>() {
+      @Override
+      @Nullable
+      public StepLog call() throws Exception {
+        final Cursor _cursor = DBUtil.query(__db, _statement, false, null);
+        try {
+          final int _cursorIndexOfUserId = CursorUtil.getColumnIndexOrThrow(_cursor, "userId");
+          final int _cursorIndexOfDate = CursorUtil.getColumnIndexOrThrow(_cursor, "date");
+          final int _cursorIndexOfCount = CursorUtil.getColumnIndexOrThrow(_cursor, "count");
+          final StepLog _result;
+          if (_cursor.moveToFirst()) {
+            final String _tmpUserId;
+            _tmpUserId = _cursor.getString(_cursorIndexOfUserId);
+            final String _tmpDate;
+            _tmpDate = _cursor.getString(_cursorIndexOfDate);
+            final int _tmpCount;
+            _tmpCount = _cursor.getInt(_cursorIndexOfCount);
+            _result = new StepLog(_tmpUserId,_tmpDate,_tmpCount);
+          } else {
+            _result = null;
+          }
+          return _result;
+        } finally {
+          _cursor.close();
+        }
+      }
+
+      @Override
+      protected void finalize() {
+        _statement.release();
+      }
+    });
+  }
+
+  @Override
+  public Object getLog(final String userId, final String date,
+      final Continuation<? super StepLog> $completion) {
+    final String _sql = "SELECT * FROM step_logs WHERE userId = ? AND date = ?";
+    final RoomSQLiteQuery _statement = RoomSQLiteQuery.acquire(_sql, 2);
+    int _argIndex = 1;
+    _statement.bindString(_argIndex, userId);
+    _argIndex = 2;
+    _statement.bindString(_argIndex, date);
+    final CancellationSignal _cancellationSignal = DBUtil.createCancellationSignal();
+    return CoroutinesRoom.execute(__db, false, _cancellationSignal, new Callable<StepLog>() {
+      @Override
+      @Nullable
+      public StepLog call() throws Exception {
+        final Cursor _cursor = DBUtil.query(__db, _statement, false, null);
+        try {
+          final int _cursorIndexOfUserId = CursorUtil.getColumnIndexOrThrow(_cursor, "userId");
+          final int _cursorIndexOfDate = CursorUtil.getColumnIndexOrThrow(_cursor, "date");
+          final int _cursorIndexOfCount = CursorUtil.getColumnIndexOrThrow(_cursor, "count");
+          final StepLog _result;
+          if (_cursor.moveToFirst()) {
+            final String _tmpUserId;
+            _tmpUserId = _cursor.getString(_cursorIndexOfUserId);
+            final String _tmpDate;
+            _tmpDate = _cursor.getString(_cursorIndexOfDate);
+            final int _tmpCount;
+            _tmpCount = _cursor.getInt(_cursorIndexOfCount);
+            _result = new StepLog(_tmpUserId,_tmpDate,_tmpCount);
+          } else {
+            _result = null;
+          }
+          return _result;
+        } finally {
+          _cursor.close();
+          _statement.release();
+        }
+      }
+    }, $completion);
+  }
+
+  @Override
+  public Flow<StepSettings> getSettings(final String userId) {
+    final String _sql = "SELECT * FROM step_settings WHERE userId = ?";
+    final RoomSQLiteQuery _statement = RoomSQLiteQuery.acquire(_sql, 1);
+    int _argIndex = 1;
+    _statement.bindString(_argIndex, userId);
+    return CoroutinesRoom.createFlow(__db, false, new String[] {"step_settings"}, new Callable<StepSettings>() {
+      @Override
+      @Nullable
+      public StepSettings call() throws Exception {
+        final Cursor _cursor = DBUtil.query(__db, _statement, false, null);
+        try {
+          final int _cursorIndexOfUserId = CursorUtil.getColumnIndexOrThrow(_cursor, "userId");
+          final int _cursorIndexOfDailyGoal = CursorUtil.getColumnIndexOrThrow(_cursor, "dailyGoal");
+          final StepSettings _result;
+          if (_cursor.moveToFirst()) {
+            final String _tmpUserId;
+            _tmpUserId = _cursor.getString(_cursorIndexOfUserId);
+            final int _tmpDailyGoal;
+            _tmpDailyGoal = _cursor.getInt(_cursorIndexOfDailyGoal);
+            _result = new StepSettings(_tmpUserId,_tmpDailyGoal);
+          } else {
+            _result = null;
+          }
+          return _result;
+        } finally {
+          _cursor.close();
+        }
+      }
+
+      @Override
+      protected void finalize() {
+        _statement.release();
+      }
+    });
+  }
+
+  @NonNull
+  public static List<Class<?>> getRequiredConverters() {
+    return Collections.emptyList();
+  }
+}
